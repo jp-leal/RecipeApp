4c5e198a6ccccbf21b3d2dc559cf4ee5908d3c61
joaobug <joaoenfrance@gmail.com>
2024-04-02 13:37:54 -0300

    Change SwiftUI List to scrollview Lazy v stack added Replaced list with foreach

--- RecipeApp/Views/RecipeListView.swift
+++ RecipeApp/Views/RecipeListView.swift
@@ -14,29 +14,37 @@ struct RecipeListView: View {
     var body: some View {
         
         NavigationStack{
-           
-                List(model.recipes) { r in
-                    
-                    NavigationLink(destination: RecipeDetailView(recipe: r), label: {
-                        
-                    HStack(spacing: 20){
-                    Image(r.image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 50, height: 50, alignment: .center)
-                        .clipped()
-                        .clipShape(.rect(cornerRadius: 5))
-                    Text(r.name)
-                } })
-            }
+            VStack(alignment: .leading){
+                Text("All Recipes")
+                    .bold()
+                    .padding(.leading)
+                    .padding(.top, 40)
+                    .font(.largeTitle)
                 
-                    
-                    
-                .navigationTitle("Recipes")
+                ScrollView{
+                    LazyVStack(alignment: .leading){
+                        ForEach(model.recipes) { r in
+                            
+                            NavigationLink(destination: RecipeDetailView(recipe: r), label: {
+                                
+                                HStack(spacing: 20){
+                                    Image(r.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                        .clipShape(.rect(cornerRadius: 5))
+                                    Text(r.name)
+                                        .foregroundStyle(.blue)
+                                } })
+                        }}
+                }
                 
-        }
+            }.padding(.leading)
+            
         }
     }
+}
 #Preview {
     RecipeListView()
         .environment(RecipeModel())
